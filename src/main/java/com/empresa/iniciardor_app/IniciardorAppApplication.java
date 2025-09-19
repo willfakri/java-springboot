68b2179a9c7add9ec97e866d74aa23d75c2e8751
Willian <willfakri@gmail.com>
2025-09-19 20:27:45 -0300

    Correção da annotation Profile

--- src/main/java/com/empresa/iniciardor_app/IniciardorAppApplication.java
+++ src/main/java/com/empresa/iniciardor_app/IniciardorAppApplication.java
@@ -2,6 +2,7 @@ package com.empresa.iniciardor_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @Profile("dev")
