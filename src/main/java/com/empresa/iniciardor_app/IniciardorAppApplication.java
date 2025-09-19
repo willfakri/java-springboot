0691cd941314bb9fae03d105dd077af0ec4282a7
Willian <willfakri@gmail.com>
2025-09-19 19:35:08 -0300

    Estrutura inicial gerada no spring initializr

---
+++ src/main/java/com/empresa/iniciardor_app/IniciardorAppApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.iniciardor_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IniciardorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IniciardorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresa/iniciardor_app/IniciardorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.iniciardor_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IniciardorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
