ad3e96d6b46b3da2c680a90b108283944082b20a
Willian <willfakri@gmail.com>
2025-09-20 08:17:04 -0300

    Retorno POST com json

--- src/main/java/com/empresa/iniciardor_app/controller/HelloWorldController.java
+++ src/main/java/com/empresa/iniciardor_app/controller/HelloWorldController.java
@@ -3,9 +3,12 @@ package com.empresa.iniciardor_app.controller;
 import com.empresa.iniciardor_app.domain.User;
 import com.empresa.iniciardor_app.service.HelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/hello")
@@ -24,9 +27,14 @@ public class HelloWorldController {
     }
 
     @PostMapping("/{id}")
-    public String helloWorld(@PathVariable("id") String id, @RequestBody User body){
-        return  "ID: " + id +
-                "\nNome: " + body.getName() +
-                "\nEmail: " + body.getEmail();
+    public ResponseEntity<Map<String, String>> helloWorld(@PathVariable("id") Integer id,
+                                                          @RequestParam(value = "filter", defaultValue = "nenhum") String filter,
+                                                          @RequestBody User body){
+        Map<String, String> response = new HashMap<>();
+        response.put("ID", id.toString());
+        response.put("Nome", body.getName());
+        response.put("Email", body.getEmail());
+        response.put("Filter", filter);
+        return ResponseEntity.ok(response);
     }
 }
