108de7a4bf0b06dc40bdec1104771f07db65bfbc
Willian <willfakri@gmail.com>
2025-09-20 08:00:20 -0300

    Método POST recebendo parâmetros

--- src/main/java/com/empresa/iniciardor_app/controller/HelloWorldController.java
+++ src/main/java/com/empresa/iniciardor_app/controller/HelloWorldController.java
@@ -1,10 +1,9 @@
 package com.empresa.iniciardor_app.controller;
 
+import com.empresa.iniciardor_app.domain.User;
 import com.empresa.iniciardor_app.service.HelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -22,5 +21,12 @@ public class HelloWorldController {
     @GetMapping
     public String helloWorld() {
         return helloWorldService.helloService("Brat");
+    }
+
+    @PostMapping("/{id}")
+    public String helloWorld(@PathVariable("id") String id, @RequestBody User body){
+        return  "ID: " + id +
+                "\nNome: " + body.getName() +
+                "\nEmail: " + body.getEmail();
     }
 }
---
+++ src/main/java/com/empresa/iniciardor_app/domain/User.java
@@ -0,0 +1,13 @@
+package com.empresa.iniciardor_app.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+    private String name;
+    private String email;
+}
