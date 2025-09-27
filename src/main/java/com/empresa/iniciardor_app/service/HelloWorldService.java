0dc10a92007af8a88a1e03cef83730d0d31da031
Willian <willfakri@gmail.com>
2025-09-27 18:51:45 -0300

    Exemplo de uso do javadoc

--- src/main/java/com/empresa/iniciardor_app/service/HelloWorldService.java
+++ src/main/java/com/empresa/iniciardor_app/service/HelloWorldService.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class HelloWorldService {
 
+    /**
+     * Exemplo de uso do recurso javadoc /**
+     * @param name
+     * @return ola nome
+     */
     public String helloService(String name){
         return "Olá, " + name;
     }
