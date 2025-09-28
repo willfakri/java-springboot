aeb3f1d5e08f5e3a1d00f57a1da37aa00089cc3a
Willian <willfakri@gmail.com>
2025-09-28 17:19:07 -0300

    Exemplo de sintaxe para for e foreach em Java.

---
+++ src/main/java/com/empresa/iniciardor_app/IniciadorConsoleApplication.java
@@ -0,0 +1,25 @@
+package com.empresa.iniciardor_app;
+
+/**
+ * Revisão da sintaxe para loop em array em Java
+ */
+public class IniciadorConsoleApplication {
+    public static void main(String[] args) {
+        String[] nomes = new String[5];
+        nomes[0] = "Pudim";
+        nomes[1] = "Simba";
+        nomes[2] = "Smeagol";
+        nomes[3] = "Careca";
+        nomes[4] = "Lady";
+
+        //Formato For padrão
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println("FOR: " + nomes[i]);
+        }
+
+        //Formato Foreach
+        for (String nome : nomes) {
+            System.out.println("FOREACH: " + nome);
+        }
+    }
+}
